49ba5d31ffebd02868611d8ad849b28055ff589d
endeavoursliet <56358803+endeavoursliet@users.noreply.github.com>
2019-12-23 21:02:56 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-$(document).ready(function(){
-   
-    $nav=$('.nav');
-    $toggleCollapse=$('.toggle-collapse');
-    
-    /**click event on toggle menu*/
-    $toggleCollapse.click(function(){
-        $nav.toggleClass('collapse');
-    })
+$(document).ready(function() {
+	$nav = $('.nav');
+	$toggleCollapse = $('.toggle-collapse');
+	const navLinks = document.querySelectorAll('.nav-link');
+
+	navLinks.forEach(link => {
+		link.addEventListener('click', () => {
+			$nav.toggleClass('collapse');
+		});
+	});
+
+	/**click event on toggle menu*/
+	$toggleCollapse.click(function() {
+		$nav.toggleClass('collapse');
+	});
 });
